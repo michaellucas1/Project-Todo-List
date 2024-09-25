dd681ff403006ba6131f4403618be95f835aed02
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-09-25 17:27:54 -0400

    add template files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// index.js
-import { greeting } from "./greeting.js";
-
-console.log(greeting);
+import "./styles/index.css";
+import "./styles/css-reset.css";
+import "./styles/modern-normalize.css";
+console.log("Hello World")
