76fb5368a5ccd23bb0de7f9b3ca3333e76f34d44
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-09-25 17:10:09 -0400

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules:[
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+
+        },
+    ],
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+// webpack.config.dev.js
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common,{
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles:["./src/template.html", "./src/styles/index.css"],
+  },
+  
+});
