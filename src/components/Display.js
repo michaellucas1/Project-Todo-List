bb7f6a0a51bcaeff7328196765066790e268652a
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-06 21:30:06 -0400

    add the import for the newly included modules and codes to display them to the dom

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,9 @@
 import Header from "./Header.js";
 import ImageAttribution from "./AttributionElement.js";
 import Connection from "./Connection.js";
+import Nav from "./Nav.js";
+import EntryForm from "./EntryForm.js";
+import Content from "./Content.js";
  export default class Display extends Connection{
     constructor(){
         super();
@@ -9,11 +12,30 @@ import Connection from "./Connection.js";
     initialize(){
         this.imageAttribution = new ImageAttribution();
         this.header = new Header();
+        this.nav = new Nav();
+        this.content = new Content();
+
         this.div = document.createElement('div');
+        this.main = document.createElement('main');
         this.div.classList.add("content-wrapper");
+
         this.connect(this.imageAttribution.getElement());
         this.connect(this.header.getElement(),this.div);
+
+        this.connect(this.nav.getElement(),this.main);
+        this.connect(this.content.getElement(),this.main);
+        this.connect(this.main,this.div);
         this.connect(this.div);
+
+        this.entry = new EntryForm();
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(this.entry.getData(), 'text/html');
+        const body = document.querySelector("body");
+        let fragment = document.createDocumentFragment();
+        let div =document.createElement('div');
+        div.innerHTML= doc.body.innerHTML;
+        fragment.appendChild(div);
+        body.appendChild(fragment);
     }
    
 
