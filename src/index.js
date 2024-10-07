a0af4b536ee83a23721ba69a6fd2a6942889adab
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-06 21:31:55 -0400

    add event listener to display and remove the modal for the input

--- src/index.js
+++ src/index.js
@@ -4,3 +4,31 @@ import "./styles/modern-normalize.css";
 import Display from "./components/Display.js";
 
 new Display();
+const addTask = document.querySelector(".project-control >.control-items:nth-child(1)");
+const dialog = document.querySelector('dialog');
+const form= document.querySelector('.book-form'); 
+addTask.addEventListener('click',add);
+function add(){
+    dialog.showModal();
+}
+
+function open(){
+    dialog.showModal();
+}
+function close(){
+    dialog.close();
+}
+
+const closeModal=document.querySelector(".close-modal");  
+console.log(closeModal);
+closeModal.addEventListener("click",close);
+form.addEventListener("submit",submit);
+function submit(event){
+    event.preventDefault();
+    const myFormData = new FormData(event.target);
+    const formDataObject = {};
+    myFormData.forEach((value,key)=>(formDataObject[key]=value));
+    console.log(formDataObject);
+    form.reset();
+    dialog.close();
+} 
