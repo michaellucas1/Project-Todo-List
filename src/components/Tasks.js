73a79f39030426de12418883a714a16b254c1d44
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-09 00:16:33 -0400

    add a class that uses the input object from the user to create the task object to be added to the project

---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+import Element from "./Element";
+export default class Tasks extends Element{
+    #parentDiv;
+    constructor(newTask){
+        super();
+        this.#createElements();
+        this.#setText(newTask);
+        this.#appendElements();
+    }
+    #createElements(){
+        this.#parentDiv=this.create('div');
+        this.title = this.create('h3');
+        this.dueDate = this.create('p');
+        this.description = this.create('p');
+        this.priority =this.create('div');
+    }
+    #setText(newTask){
+        this.title.textContent=`${newTask.title}`;
+        this.dueDate.textContent = `${newTask.due}`;
+        this.description.textContent = `${newTask.description}`;
+        this.priority.textContent = `${newTask.priority}`;
+    }
+    #appendElements(){
+        this.connect(this.title,this.#parentDiv);
+        this.connect(this.dueDate,this.#parentDiv);
+        this.connect(this.description,this.#parentDiv);
+        this.connect(this.priority,this.#parentDiv);
+    }
+    getElement(){
+        return this.#parentDiv;
+    }
+}
