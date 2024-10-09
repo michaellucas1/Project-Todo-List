c03469dc90698eefb06099ce0a258799bc3089ac
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-09 00:13:13 -0400

    add a class that would manage all existing projects and store it to an array. Work in progress

---
+++ src/components/ProjectManager.js
@@ -0,0 +1,12 @@
+export default class ProjectManager{
+    constructor(){
+        this.projectArray=new Array();
+        this.projectNav = document.querySelector('.project-nav');
+        this.projectMain = document.querySelector('.main-content');
+        this.project
+    }
+    addProject(newProject){
+        this.projectArray.push(newProject);
+
+    }
+}
