f0cb381dfddb68e2c14ea1473b7c821bccc0ff20
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-09 00:03:56 -0400

    refactor the code for a cleaner look

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,41 +2,56 @@ import Header from "./Header.js";
 import ImageAttribution from "./AttributionElement.js";
 import Connection from "./Connection.js";
 import Nav from "./Nav.js";
-import EntryForm from "./EntryForm.js";
+import ProjectForm from "./ProjectForm.js";
+import TaskForm from "./TaskForm.js";
 import Content from "./Content.js";
+
  export default class Display extends Connection{
     constructor(){
         super();
         this.initialize();
     }
     initialize(){
-        this.imageAttribution = new ImageAttribution();
-        this.header = new Header();
-        this.nav = new Nav();
-        this.content = new Content();
-
-        this.div = document.createElement('div');
-        this.main = document.createElement('main');
-        this.div.classList.add("content-wrapper");
-
-        this.connect(this.imageAttribution.getElement());
-        this.connect(this.header.getElement(),this.div);
-
-        this.connect(this.nav.getElement(),this.main);
-        this.connect(this.content.getElement(),this.main);
-        this.connect(this.main,this.div);
-        this.connect(this.div);
-
-        this.entry = new EntryForm();
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(this.entry.getData(), 'text/html');
-        const body = document.querySelector("body");
-        let fragment = document.createDocumentFragment();
-        let div =document.createElement('div');
-        div.innerHTML= doc.body.innerHTML;
-        fragment.appendChild(div);
-        body.appendChild(fragment);
+        this.#parentDiv();
+        this.#imageAttribute();
+        this.#header();
+        this.#main();   
+        this.#nav();
+        this.#content();
+        this.#task();
+        this.#project();
     }
-   
-
-} 
+    #header(){
+        this.header = new Header();
+        this.connect(this.header.getElement(),this.div);
+    }
+    #imageAttribute(){
+        this.imageAttribution = new ImageAttribution();
+        this.connect(this.imageAttribution.getElement());
+    }
+    #content(){
+        this.content = new Content();
+        this.connect(this.content.getElement(),this.main);
+    }
+    #project(){
+        this.project = new ProjectForm();
+        this.connect(this.project.getElement(),this.div);
+    }
+    #task(){
+        this.task = new TaskForm();
+        this.connect(this.task.getElement(),this.div);
+    }
+    #parentDiv(){
+        this.div = document.createElement('div');
+        this.div.classList.add("content-wrapper");
+        this.connect(this.div);
+    }
+    #nav(){
+        this.nav = new Nav();
+        this.connect(this.nav.getElement(),this.main);
+    }
+    #main(){
+        this.main = document.createElement('main');
+        this.connect(this.main,this.div);
+    }
+}
