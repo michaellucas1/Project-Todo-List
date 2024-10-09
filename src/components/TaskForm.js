3a361a7cb2dc6b6c2d1fa708e7a907dde873840f
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-09 00:14:58 -0400

    add a class that handles the creation of a modal that allow the user to add more task to a project

---
+++ src/components/TaskForm.js
@@ -0,0 +1,47 @@
+export default class EntryForm {
+    constructor(){
+        this.taskForm = `
+            <dialog class="task-dialog">
+                <div class="modal-container">
+                    <button type="button" class="close-task-modal">&#10006;</button>
+                    <form class="task-form">
+                        <h1>New Task</h1>
+                        <div class="form-control-container">
+                            <div class="form-control">
+                                <label for="title">Title:</label>
+                                <input type="text" id="title" name="title" maxlength="64" required>
+                            </div>
+                            <div class="form-control">
+                                <label for="due">Due Date:</label>
+                                <input type="date" id="due" name="due" required>
+                            </div>
+                            <div class="form-control">
+                                <label for="description">Description:</label>
+                                <textarea id="description" name="description" maxlength="64" required rows="5" cols="33"></textarea>
+                            </div>
+                            <div class="form-control">
+                                <label for="priority">Priority</label>
+                                <select name="priority" id="priority">
+                                    <option value="High-Priority">High Priority</option>
+                                    <option value="Neutral">Neutral</option>
+                                    <option value="Low-Priority">Low Priority</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button class="submit-button" type=submit>Submit</button>
+                    </form>
+                </div>
+            </dialog>`
+        this.createForm(this.taskForm);
+    }
+    createForm(formData){
+        this.parser = new DOMParser();
+        this.doc = this.parser.parseFromString(formData, 'text/html');
+        this.div =document.createElement('div');
+        this.div.innerHTML= this.doc.body.innerHTML;
+    }
+    getElement(){
+        return this.div;
+    }
+
+} 
