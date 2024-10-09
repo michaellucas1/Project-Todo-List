314900fe4867453a1f5d05d242ffabcb7f576530
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-09 00:06:56 -0400

    refactor the event listener and move it to its own class and create an instance of it

--- src/index.js
+++ src/index.js
@@ -2,33 +2,7 @@ import "./styles/index.css";
 import "./styles/css-reset.css";
 import "./styles/modern-normalize.css";
 import Display from "./components/Display.js";
+import EventListeners from "./components/EventListeners.js"
 
 new Display();
-const addTask = document.querySelector(".project-control >.control-items:nth-child(1)");
-const dialog = document.querySelector('dialog');
-const form= document.querySelector('.book-form'); 
-addTask.addEventListener('click',add);
-function add(){
-    dialog.showModal();
-}
-
-function open(){
-    dialog.showModal();
-}
-function close(){
-    dialog.close();
-}
-
-const closeModal=document.querySelector(".close-modal");  
-console.log(closeModal);
-closeModal.addEventListener("click",close);
-form.addEventListener("submit",submit);
-function submit(event){
-    event.preventDefault();
-    const myFormData = new FormData(event.target);
-    const formDataObject = {};
-    myFormData.forEach((value,key)=>(formDataObject[key]=value));
-    console.log(formDataObject);
-    form.reset();
-    dialog.close();
-} 
+new EventListeners();
