3414ff408027d31c45f581bb939d10b85245ab11
michaellucas1 <141792618+michaellucas1@users.noreply.github.com>
2024-10-15 00:19:57 -0400

    add a method that create element that will be displayed on the nav bar and initialize the project with a default project

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -27,5 +27,4 @@ export default class EntryForm {
     getElement(){
         return this.div;
     }
-
 } 
--- src/components/ProjectManager.js
+++ src/components/ProjectManager.js
@@ -4,16 +4,65 @@ import GenerateID from './GenerateID.js';
 export default class ProjectManager{
     constructor(){
         this.idGenerator = new GenerateID();
-        this.projectNav = document.querySelector('.project-nav');
-        this.projectMain = document.querySelector('.main-content');
+        this.nav = document.querySelector('nav');
+        this.projectMain = document.querySelector('.project-item-container');
+        this.taskFormOption = document.getElementById('locale');
+
+        this.default = new Project(this.defaultProject());
+        this.createElement(this.defaultProject().ID);
+        this.projectNav.textContent=this.defaultProject().project;
+
+        this.nav.appendChild(this.projectNav);
+        this.projectMain.append(this.default.getElement());
+        
+    }
+    defaultProject(){
+        const defaultProject={
+            project:'Default Project',
+            ID:`default`,
+        };
+        return defaultProject;
     }
     addProject(newProject){
-        const project = new Project(newProject)
-        project.setID(this.idGenerator.generateID());
-        const taskFormOption = document.getElementById('locale');
+        this.setID(newProject);
+        this.createElement(newProject.ID);
+        console.log(newProject.ID)
+        const project = new Project(newProject);
+        newProject['element'] = project.getElement();
         project.getOption().setAttribute("value",project.getID());
-        taskFormOption.appendChild(project.getOption());
+        this.taskFormOption.appendChild(project.getOption());
+        this.projectNav.textContent= newProject.project;
+        this.nav.appendChild(this.projectNav);
+        this.projectNav.addEventListener('click',this.displayProject.bind(this));
         array.push(project.getElement());
-        console.log(project.getID());
+    }
+    displayProject(event){
+        this.projectMain.innerHTML='';
+        this.projectMain.appendChild(this.getProject(event.target.id));
+
+
+    }
+    getProject(id){
+        let result;
+        for(let i =0;i<array.length;i++){
+            if(array[i].id===id){
+                result=array[i];
+                break;
+            }
+        }
+        return  result;
+    }
+    setID(newProject){
+        if(newProject.hasOwnProperty('ID')===false){
+            newProject['ID']=this.idGenerator.generateID();
+        }
+    }
+    createElement(id){
+        this.projectNav = document.createElement('div');
+        this.taskContainer=document.createElement('div');
+        this.projectNav.classList.add('project-nav');
+        this.projectNav.id=id;
+        this.taskContainer.classList.add('task-icon-container');
+        this.projectNav.appendChild(this.taskContainer);
     }
 }
